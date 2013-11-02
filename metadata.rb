bef51062882d5c2814c4babb354157055e75c995
Andrey Chernih <andrey.chernih@gmail.com>
2013-11-02 13:11:51 +0400

    Add license information

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'secure-server'
 maintainer       'Andrey Chernih'
 maintainer_email 'andrey.chernih@gmail.com'
-license          'All rights reserved'
+license          'Apache License, Version 2.0'
 description      'Make Linux server relatively secure'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
